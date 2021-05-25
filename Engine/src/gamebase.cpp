b245d3ccf069690183341b09831c8e5cff5376e9
IkerNoya <ikernoya4@gmail.com>
2021-05-25 16:54:04 -0300

    Imported imGUI and added basic test gui

--- Engine/src/gamebase.cpp
+++ Engine/src/gamebase.cpp
@@ -5,6 +5,9 @@
 
 #include "GLFW/glfw3.h"
 
+#include "gui/imgui.h"
+#include "gui/imgui_impl_glfw_gl3.h"
+
 Gamebase::Gamebase() {
 	window = new Window(1280, 720);
 	renderer = new Renderer();
@@ -49,47 +52,66 @@ int Gamebase::initEngine() {
     renderer->setPositionAttribPointer(basicShader.getID(), "pos");
     renderer->setTintAttribPointer(basicShader.getID(), "color");
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui_ImplGlfwGL3_Init(window->getWindow(), true);
+
+    ImGui::StyleColorsDark();   
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
 
 	return 0;
 }
 
 void Gamebase::updateEngine() {
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     bool wireMode = false;
 	while (!glfwWindowShouldClose(window->getWindow())) {
 		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        ImGui_ImplGlfwGL3_NewFrame();
+
 		renderer->startProgram(basicShader);
         renderer->bindVAO(_vao);
-        if (glfwGetKey(window->getWindow(), GLFW_KEY_SPACE) == GLFW_PRESS && !wireMode) {
+        if (wireMode) {
             renderer->activateWireframeMode();
-            wireMode = true;
         }
-        else if (glfwGetKey(window->getWindow(), GLFW_KEY_SPACE) == GLFW_PRESS && wireMode) {
+        else{
             renderer->deactivateWireframeMode();
-            wireMode = false;
         }
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		update();
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            if (ImGui::Button("WIREFRAME"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                wireMode = !wireMode;
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window->getWindow());
 		glfwPollEvents();
+         
 	}
 }
 
 void Gamebase::unloadEngine() {
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     renderer->deleteBuffers(_vao, _vbo, _ebo);
     glDeleteProgram(basicShader.getID());
 	glfwTerminate();
