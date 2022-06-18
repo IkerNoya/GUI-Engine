b7b4520e1b9f24a22e240200e1be666d8de2a6f3
Iker Noya <ikernoya4@gmail.com>
2022-06-17 21:13:11 -0300

    Changed world data UI

--- Engine/src/world_data.cpp
+++ Engine/src/world_data.cpp
@@ -15,9 +15,14 @@ void WorldData::createWindow() {
 		return;
 	}
 	for (int i = 0; i < _dataManager->getLevelEntities().size(); i++) {
-		if (ImGui::Button(_dataManager->getLevelEntities()[i]->GetName().c_str(), ImVec2(ImGui::GetWindowWidth(), 50))) {
+		if (ImGui::TreeNode(_dataManager->getLevelEntities()[i]->GetName().c_str())) {
 			_dataManager->setSelectedEntity(_dataManager->getLevelEntities()[i]);
+			ImGui::TreePop();
 		}
+		//ImGui::TreePop();
+	/*	if (ImGui::Button(_dataManager->getLevelEntities()[i]->GetName().c_str(), ImVec2(ImGui::GetWindowWidth(), 50))) {
+			_dataManager->setSelectedEntity(_dataManager->getLevelEntities()[i]);
+		}*/
 		ImGui::Spacing();
 	}
 	ImGui::End();
